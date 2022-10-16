6577f87f9294a7d9d6142f8d48e32a9543c6b36c
liuywx <liuywx@yonyou.com>
2022-10-16 19:21:44 +0800

    修改module path

--- yonbip/open/api/sdk/token_opt.go
+++ yonbip/open/api/sdk/token_opt.go
@@ -3,100 +3,101 @@ package sdk
 import (
 	"encoding/json"
 	"github.com/coocood/freecache"
+	"github.com/tmzjlyw/yonyou-openapi-sdk/yonbip/open/utils"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
 	"time"
-	"yonyou-openapi-sdk/yonbip/open/utils"
 )
- var cache = freecache.NewCache(10 * 1024 * 1024)
 
+var cache = freecache.NewCache(10 * 1024 * 1024)
 
 type tokenReqInfo struct {
-	Code string  `json:"code"`
-	Message string  `json:"message"`
-	Data tokenInfo  `json:"data"`
+	Code    string    `json:"code"`
+	Message string    `json:"message"`
+	Data    tokenInfo `json:"data"`
 }
 
 type tokenInfo struct {
-	AccessToken string  `json:"access_token"`
-	Expire int  `json:"expire"`
+	AccessToken string `json:"access_token"`
+	Expire      int    `json:"expire"`
 }
 
- //获取
+//获取
 func OptSelfToken(appKey string, appSecret string, hostUrl string) string {
-	tokenParamDic :=make(map[string]string)
-	tokenParamDic["appKey"]=appKey
-	tokenParamDic["timestamp"]= strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
-	return string(OptTokenRequest(hostUrl+ "/iuap-api-auth/open-auth/selfAppAuth/getAccessToken",tokenParamDic,appSecret))
+	tokenParamDic := make(map[string]string)
+	tokenParamDic["appKey"] = appKey
+	tokenParamDic["timestamp"] = strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	return string(OptTokenRequest(hostUrl+"/iuap-api-auth/open-auth/selfAppAuth/getAccessToken", tokenParamDic, appSecret))
 }
 
 func OptSuiteToken(suiteKey string, tenantId string, appSecret string, hostUrl string) string {
-	tokenParamDic :=make(map[string]string)
-	tokenParamDic["suiteKey"]=suiteKey
-	tokenParamDic["tenantId"]=tenantId
-	tokenParamDic["timestamp"]= strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
-	return string(OptTokenRequest(hostUrl+ "/iuap-api-auth/open-auth/suiteApp/getAccessToken",tokenParamDic,appSecret))
+	tokenParamDic := make(map[string]string)
+	tokenParamDic["suiteKey"] = suiteKey
+	tokenParamDic["tenantId"] = tenantId
+	tokenParamDic["timestamp"] = strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	return string(OptTokenRequest(hostUrl+"/iuap-api-auth/open-auth/suiteApp/getAccessToken", tokenParamDic, appSecret))
 
 }
 func OptSelfTokenWithCache(appKey string, appSecret string, hostUrl string) string {
-	key:=appKey
+	key := appKey
 	value, _ := cache.Get([]byte(key))
-	if value !=nil {
+	if value != nil {
 		return string(value)
 	}
-	tokenParamDic :=make(map[string]string)
-	tokenParamDic["appKey"]=appKey
-	tokenParamDic["timestamp"]= strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
-	result := OptTokenRequest(hostUrl+"/iuap-api-auth/open-auth/selfAppAuth/getAccessToken",tokenParamDic,appSecret)
+	tokenParamDic := make(map[string]string)
+	tokenParamDic["appKey"] = appKey
+	tokenParamDic["timestamp"] = strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	result := OptTokenRequest(hostUrl+"/iuap-api-auth/open-auth/selfAppAuth/getAccessToken", tokenParamDic, appSecret)
 	tokenResult := tokenReqInfo{}
-	err := json.Unmarshal(result, &tokenResult);
-	if  err == nil {
-		accessToken := tokenResult.Data.AccessToken;
-		cache.Set([]byte(key),[]byte(accessToken),tokenResult.Data.Expire/1000-10)
-		return accessToken	}
-	panic(err)
-}
-
-func OptSuiteTokenWithCache(suiteKey string, tenantId string, suiteSecret string, hostUrl string) string {
-	key:=suiteKey+tenantId
-	value, _ := cache.Get([]byte(key))
-	if value !=nil {
-		return string(value)
-	}
-	tokenParamDic :=make(map[string]string)
-	tokenParamDic["suiteKey"]=suiteKey
-	tokenParamDic["tenantId"]=tenantId
-	tokenParamDic["timestamp"]= strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
-	result := OptTokenRequest(hostUrl+"/iuap-api-auth/open-auth/suiteApp/getAccessToken",tokenParamDic, suiteSecret)
-	tokenResult := tokenReqInfo{}
-	err := json.Unmarshal(result, &tokenResult);
-	if  err == nil {
-		accessToken := tokenResult.Data.AccessToken;
-		cache.Set([]byte(key),[]byte(accessToken),tokenResult.Data.Expire/1000-10)
+	err := json.Unmarshal(result, &tokenResult)
+	if err == nil {
+		accessToken := tokenResult.Data.AccessToken
+		cache.Set([]byte(key), []byte(accessToken), tokenResult.Data.Expire/1000-10)
 		return accessToken
 	}
 	panic(err)
 }
 
-func OptTokenRequest(requestUrl string,tokenParamDic map[string]string, appSecret string) []byte {
+func OptSuiteTokenWithCache(suiteKey string, tenantId string, suiteSecret string, hostUrl string) string {
+	key := suiteKey + tenantId
+	value, _ := cache.Get([]byte(key))
+	if value != nil {
+		return string(value)
+	}
+	tokenParamDic := make(map[string]string)
+	tokenParamDic["suiteKey"] = suiteKey
+	tokenParamDic["tenantId"] = tenantId
+	tokenParamDic["timestamp"] = strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	result := OptTokenRequest(hostUrl+"/iuap-api-auth/open-auth/suiteApp/getAccessToken", tokenParamDic, suiteSecret)
+	tokenResult := tokenReqInfo{}
+	err := json.Unmarshal(result, &tokenResult)
+	if err == nil {
+		accessToken := tokenResult.Data.AccessToken
+		cache.Set([]byte(key), []byte(accessToken), tokenResult.Data.Expire/1000-10)
+		return accessToken
+	}
+	panic(err)
+}
+
+func OptTokenRequest(requestUrl string, tokenParamDic map[string]string, appSecret string) []byte {
 	sortData := utils.SortTokenParam(tokenParamDic)
 	sign := utils.EncoderSha256(appSecret, sortData)
 	tokenParamDic["signature"] = sign
 	uri, err := url.Parse(requestUrl)
-	if err !=nil{
+	if err != nil {
 		panic(err)
 	}
 	values := url.Values{} //拼接query参数
-	for k,v := range tokenParamDic {
+	for k, v := range tokenParamDic {
 		values.Add(k, v)
 	}
 	uri.RawQuery = values.Encode()
 	urlPath := uri.String()
-	resp,err := http.Get(urlPath)
-	if err !=nil{
+	resp, err := http.Get(urlPath)
+	if err != nil {
 		panic(err)
 	}
 	defer func(Body io.ReadCloser) {
@@ -106,5 +107,5 @@ func OptTokenRequest(requestUrl string,tokenParamDic map[string]string, appSecre
 		}
 	}(resp.Body)
 	body, _ := ioutil.ReadAll(resp.Body)
-    return body
+	return body
 }
--- yonbip/open/event/sdk/event_opt.go
+++ yonbip/open/event/sdk/event_opt.go
@@ -5,17 +5,17 @@ import (
 	"crypto/cipher"
 	"encoding/base64"
 	"encoding/json"
+	"github.com/tmzjlyw/yonyou-openapi-sdk/yonbip/open/utils"
 	"strings"
-	"yonyou-openapi-sdk/yonbip/open/utils"
 )
 
 func DecryptEventEncrypt(appSecret string, holder map[string]string) string {
 	holderInfoList := []string{holder["nonce"], holder["encrypt"], holder["timestamp"]}
 	sortedData := utils.SortListParam(holderInfoList)
 	sha256 := utils.EncoderSha256(appSecret, sortedData)
-	secret := buildAesKeyFromSecret(appSecret)+"="
-	secretBytes, _ :=  base64.StdEncoding.DecodeString(secret)
-	encrypt, _ :=  base64.StdEncoding.DecodeString(holder["encrypt"])
+	secret := buildAesKeyFromSecret(appSecret) + "="
+	secretBytes, _ := base64.StdEncoding.DecodeString(secret)
+	encrypt, _ := base64.StdEncoding.DecodeString(holder["encrypt"])
 	if sha256 == holder["signature"] {
 		return AesDecryptCBC(encrypt, secretBytes)
 	}
@@ -24,36 +24,36 @@ func DecryptEventEncrypt(appSecret string, holder map[string]string) string {
 }
 
 func AesDecryptCBC(encrypted []byte, key []byte) string {
-	block, er := aes.NewCipher(key)        // 分组秘钥
+	block, er := aes.NewCipher(key) // 分组秘钥
 	if er != nil {
 		panic(er)
 	}
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) // 加密模式
 	//encrypted = MakeBlocksFull(encrypted,blockSize)
-	decrypted := make([]byte, len(encrypted))                   // 创建数组
-	blockMode.CryptBlocks(decrypted, encrypted)                 // 解密
+	decrypted := make([]byte, len(encrypted))   // 创建数组
+	blockMode.CryptBlocks(decrypted, encrypted) // 解密
 	padding := pkcs5UnPadding(decrypted)
-	dataMapJson , _ := json.Marshal(padding)
+	dataMapJson, _ := json.Marshal(padding)
 	dataString := string(dataMapJson)
 	return dataString
 
 }
 
 func pkcs5UnPadding(origData []byte) map[string]string {
-    le := len(origData)
+	le := len(origData)
 	padIndex := int(string(origData)[le-2])
-	if padIndex < 1 || padIndex > 32{
+	if padIndex < 1 || padIndex > 32 {
 		padIndex = 0
 	}
 
-	origData = origData[0:le -padIndex]
+	origData = origData[0 : le-padIndex]
 	// 分离16位随机字符串, 网络字节序和corpId
 	networkOrder := origData[16:20]
 	xmlLength := recoverNetworkBytesOrder(networkOrder)
-	plainInfo := origData[20:20 + xmlLength]
-	suitKey := origData[20 + xmlLength:]
-	return map[string]string {"event_info": string(plainInfo), "suit_key": string(suitKey)}
+	plainInfo := origData[20 : 20+xmlLength]
+	suitKey := origData[20+xmlLength:]
+	return map[string]string{"event_info": string(plainInfo), "suit_key": string(suitKey)}
 }
 
 func buildAesKeyFromSecret(appSecret string) string {
@@ -77,7 +77,7 @@ func recoverNetworkBytesOrder(order_bytes []byte) int {
 	source_number := 0
 
 	j := 0
-	for ; j < 4; {
+	for j < 4 {
 		source_number <<= 8
 		source_number |= int(order_bytes[j]) & 0xff
 		j++
@@ -85,4 +85,3 @@ func recoverNetworkBytesOrder(order_bytes []byte) int {
 	return source_number
 
 }
-
